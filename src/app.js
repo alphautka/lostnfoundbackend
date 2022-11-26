cca085bd0ac5ab4f01182d064c548c3bc60f6a65
Andrey Ranenko <andrey.ranenko@kinetyx.tech>
2022-11-26 13:09:47 -0700

    base auth

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import {pool} from './db.js';
 
 
 const app = express();
+app.use(express.json());
 app.use(cors());
 app.use(morgan('common'));
 
@@ -15,11 +16,22 @@ app.get("/", (req, res) => {
 });
 
 app.post('/login', (req, res) => {
-    pool.query('SELECT * from users', (error, results, fields) => {
+    const {username, password} = req.body;
+    if (!username || !password) {
+        // throw error
+    }
+
+    pool.query(`SELECT * from users where username='${username}'`, (error, results, fields) => {
         if (!!error) {
             res.send(error);
         } else {
-            res.send(results[0]);
+            if (results.length && results[0].password === password) {
+                res.send(results[0]);
+            } else {
+                res.status(401)
+                res.send('unauthorized');
+            }
+            // res.send(results);
         }
     });
 })
